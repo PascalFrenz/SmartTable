c1b18e5e2933055e3152e6ca264755ed8f63306f
Pascal <TimeRunsFast@github.com>
2017-09-17 22:29:31 +0200

    Changed type to RecyclerView.Adapter; Supports rearranging items

--- app/src/main/java/org/thecoders/smarttable/ui/Adapter_Lesson.kt
+++ app/src/main/java/org/thecoders/smarttable/ui/Adapter_Lesson.kt
@@ -1,134 +1,102 @@
 package org.thecoders.smarttable.ui
 
 import android.content.Context
-import android.graphics.Rect
-import android.support.constraint.ConstraintLayout
+import android.graphics.Color
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.Space
-import android.widget.TextView
 import kotlinx.android.synthetic.main.listview_item_lesson.view.*
+import org.thecoders.smarttable.LessonItemTouchHelperAdapter
+import org.thecoders.smarttable.LessonItemTouchHelperViewHolder
 import org.thecoders.smarttable.R
-import org.thecoders.smarttable.RearrangeableListView
 import org.thecoders.smarttable.TimeHelper
 import org.thecoders.smarttable.data.Lesson
+import java.util.*
 
 /**
  * Created by frenz on 30.07.2017.
  */
 
-class Adapter_Lesson(context: Context, val layoutResourceId: Int,
-                     var data: MutableList<Lesson>, val enableEdit: Boolean) :
-        ArrayAdapter<Lesson>(context, layoutResourceId, data), RearrangeableListView.RearrangeListener {
-
-    private class LessonHolder {
-        lateinit var mTimespan: TextView
-        lateinit var mSubject: TextView
-        lateinit var mTeacher: TextView
-        lateinit var mDragspace: Space
-        lateinit var mDeleteIcon: ImageView
-    }
+class Adapter_Lesson(val context: Context, var data: MutableList<Lesson>, val enableEdit: Boolean) :
+        RecyclerView.Adapter<Adapter_Lesson.LessonHolder>(), LessonItemTouchHelperAdapter {
 
     companion object {
         private val LOG_TAG = Adapter_Lesson::class.java.simpleName
     }
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var row = convertView
-        val holder: LessonHolder
+    inner class LessonHolder(itemView: View) : RecyclerView.ViewHolder(itemView), LessonItemTouchHelperViewHolder {
 
-        if(row == null) {
-            holder = LessonHolder()
-            row = LessonView(context, holder)
+        fun bind(position: Int) {
+            val lesson = data[position]
+            val mTimespan = itemView.item_lesson_timespan
+            val mSubject = itemView.item_lesson_subject
+            val mDeleteIcon = itemView.item_lesson_delete
 
-            initHolder(holder, row)
-            row.tag = holder
+            mTimespan.text = lesson.timing
+            mSubject.text = lesson.subjectName
+
+            if(enableEdit) {
+                mDeleteIcon.setOnClickListener {
+                    data.removeAt(position)
+                    notifyItemRemoved(position)
+                }
+            } else {
+                mDeleteIcon.isClickable = false
+                mDeleteIcon.visibility = View.INVISIBLE
+            }
+        }
+
+        override fun onItemSelected() {
+            itemView.setBackgroundColor(Color.LTGRAY)
+        }
+
+        override fun onItemClear() {
+            itemView.setBackgroundColor(0)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LessonHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(
+                R.layout.listview_item_lesson,
+                parent,
+                false
+        )
+
+        return LessonHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: LessonHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int = data.count()
+
+    override fun onItemMove(fromPosition: Int, toPosition: Int): Boolean {
+        if(fromPosition < toPosition) {
+            for (i in fromPosition until toPosition) Collections.swap(data, i, i + 1)
+            data[fromPosition].id--
+            data[toPosition].id++
         } else {
-            holder = row.tag as LessonHolder
+            for (i in fromPosition downTo toPosition + 1) Collections.swap(data, i, i - 1)
+            data[fromPosition].id++
+            data[toPosition].id--
         }
 
-        val lesson = data[position]
+        data[fromPosition].timing = TimeHelper.calcTimingById(context, data.indexOf(data[fromPosition]))
+        data[toPosition].timing = TimeHelper.calcTimingById(context, data.indexOf(data[toPosition]))
 
-        setupHolder(holder, lesson, row)
+        notifyItemChanged(fromPosition)
+        notifyItemChanged(toPosition)
 
-        return row
+        notifyItemMoved(fromPosition, toPosition)
+
+        return true
     }
 
-    override fun notifyDataSetChanged() {
-        super.notifyDataSetChanged()
-
-        data.forEach {
-            it.id = getPosition(it).toLong()
-            it.timing = TimeHelper().calcTimingById(context, it.id.toInt())
-        }
-
-//        var i = 0
-//        while(i < count) {
-//            val lesson = getItem(i)
-//            lesson.id = i.toLong()
-//            lesson.timing = TimeHelper().calcTimingById(context, i)
-//            Log.v(LOG_TAG, lesson.toString())
-//            i++
-//        }
-
-    }
-
-    private fun initHolder(holder: LessonHolder, row: View) {
-        holder.mTimespan = row.item_lesson_timespan
-        holder.mSubject = row.item_lesson_subject
-        holder.mTeacher = row.item_lesson_teacher
-        holder.mDragspace = row.item_lesson_dragspace
-        holder.mDeleteIcon = row.item_lesson_delete
-    }
-
-    private fun setupHolder(holder: LessonHolder, lesson: Lesson, row: View) {
-        holder.mTimespan.text = lesson.timing
-        holder.mSubject.text = lesson.subjectName
-
-        if(enableEdit) {
-            holder.mDeleteIcon.setOnClickListener { remove(lesson) }
-        } else {
-            holder.mDeleteIcon.isClickable = false
-            holder.mDeleteIcon.visibility = View.INVISIBLE
-        }
-    }
-
-    override fun onGrab(index: Int) { }
-
-    override fun onRearrangeRequested(fromIndex: Int, toIndex: Int): Boolean {
-        if(toIndex in 0..(count - 1)) {
-            val dragItem = getItem(fromIndex)
-
-            remove(dragItem)
-            insert(dragItem, toIndex)
-
-            notifyDataSetChanged()
-
-            return true
-        }
-        return false
-    }
-
-    override fun onDrop() { }
-
-    private inner class LessonView(context: Context, internal val holder: LessonHolder) :
-            ConstraintLayout(context), RearrangeableListView.MovableView {
-
-        init {
-            View.inflate(context, R.layout.listview_item_lesson, this)
-        }
-
-        override fun onGrabAttempt(x: Int, y: Int): Boolean {
-            val dragSpace = Rect()
-            holder.mDragspace.getHitRect(dragSpace)
-            return dragSpace.contains(x, y)
-        }
-
-        override fun onRelease() {
-
-        }
-
+    fun alterItems(newList: List<Lesson>) {
+        data = newList.toMutableList()
+        notifyDataSetChanged()
     }
 }
