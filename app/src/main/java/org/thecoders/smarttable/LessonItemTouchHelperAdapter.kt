8c88fbe18eb71338586da0970b5d6e6194036281
Pascal <TimeRunsFast@github.com>
2017-09-17 22:28:56 +0200

    Initial commit

---
+++ app/src/main/java/org/thecoders/smarttable/LessonItemTouchHelperAdapter.kt
@@ -0,0 +1,10 @@
+package org.thecoders.smarttable
+
+/**
+ * Created by frenz on 14.09.2017.
+ */
+interface LessonItemTouchHelperAdapter {
+
+    fun onItemMove(fromPosition: Int, toPosition: Int): Boolean
+
+}
